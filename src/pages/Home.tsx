167e8da56c3dc664ac23beff486c1e2a0094bbe0
Lars Pier De Las Salas <larspier@hotmail.com>
2024-11-10 11:05:55 -0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -145,6 +145,10 @@ export default function Home() {
                     </div>
                     <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                         <article className="flex max-w-xl flex-col items-start justify-between">
+                          <div class="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96">
+                            <img alt="" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=3603&amp;q=80" 
+                              class="object-cover w-full h-full">
+                          </div>
                             <div className="flex items-center gap-x-4 text-xs">
                                 <time dateTime="2024-11-06" className="text-gray-500">Nov 6, 2024</time>
                                 <a href="#"
